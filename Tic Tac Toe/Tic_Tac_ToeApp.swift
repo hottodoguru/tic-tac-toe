d5f1860617b7d0778e9c94f81fbf6bea007836a0
Chanoknun Choosaksilp <derpy33074@gmail.com>
2021-08-23 16:14:50 +0700

    Initial Commit

---
+++ Tic Tac Toe/Tic_Tac_ToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tic_Tac_ToeApp.swift
+//  Tic Tac Toe
+//
+//  Created by Chanoknun Choosaksilp on 23/8/2564 BE.
+//
+
+import SwiftUI
+
+@main
+struct Tic_Tac_ToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
